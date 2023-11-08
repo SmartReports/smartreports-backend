e99f09a9570a7b696a463b030ef11390ea00dcf1
matteotolloso <matteotolloso@gmail.com>
2023-11-08 16:48:04 +0100

    fix

--- server_run.sh
+++ server_run.sh
@@ -1,6 +1,6 @@
 pip3 install -r requirements.txt
 pip3 freeze > requirements.txt
 python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py migrate --run-syncdb 
+python3 manage.py migrate --fake
+python3 manage.py migrate --run-syncdb --fake
 python3 manage.py runserver 
