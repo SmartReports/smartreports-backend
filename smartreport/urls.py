b3215fe8695adbe1936a775836aa817d4db3c8cd
PaulMagos <p.magos@studenti.unipi.com>
2023-11-11 12:51:46 +0100

    Fix Migrations and static files folder collect

--- smartreport/urls.py
+++ smartreport/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.conf import settings
 # from django.conf.urls.static import static
 
@@ -23,3 +25,4 @@ urlpatterns = [
     path("", include("smartreport_app.urls")),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
