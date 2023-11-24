77d90499142f33e678da58488365907b69473028
Claudia <cledi.gent@gmail.com>
2023-11-24 23:25:12 +0100

    email

--- smartreport/settings.py
+++ smartreport/settings.py
@@ -190,8 +190,8 @@ DEBUG = True
 DEFAULT_FROM_EMAIL = 'smartreports@example.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_PORT = 2525
-EMAIL_HOST_USER = 'fa29b0a6d18965'
-EMAIL_HOST_PASSWORD = '84f5260599344e'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
--- smartreport_app/email.py
+++ smartreport_app/email.py
@@ -2,56 +2,58 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 from .models import UserType, ArchivedReport, Email, ReportTemplate
 
-def send_emails_for_unsent_reports():
-    # Get distinct user types from UserType enumeration
-    user_types = [user_type[0] for user_type in UserType.choices]
+class ReportEmail():
+    @staticmethod
+    def send_emails_for_unsent_reports():
+        # Get distinct user types from UserType enumeration
+        user_types = [user_type[0] for user_type in UserType.choices]
 
-    for user_type in user_types:
-        try:
-            # Get the unsent ArchivedReport instances for the user type
-            unsent_reports = ArchivedReport.objects.filter(
-                user_type=user_type, sent=False
-            )
+        for user_type in user_types:
+            try:
+                # Get the unsent ArchivedReport instances for the user type
+                unsent_reports = ArchivedReport.objects.filter(
+                    user_type=user_type, sent=False
+                )
 
-            # Get the corresponding Email instance for the user type
-            email_instance = Email.objects.get(user_type=user_type)
+                # Get all corresponding Email instances for the user type
+                email_instances = Email.objects.filter(user_type=user_type)
 
-            # Validate that emails field is a list
-            if not isinstance(email_instance.emails, list):
-                raise ValidationError("Emails must be a list")
+                # Validate that emails field is a list
+                for email_instance in email_instances:
+                    if not isinstance(email_instance.emails, list):
+                        raise ValidationError("Emails must be a list")
 
-            # Iterate over each unsent report and send an email for each file
-            for report in unsent_reports:
-                # Check if the report template matches the user's template
-                if report.template.user_type == user_type:
-                    # Create an EmailMessage object
-                    subject = 'Subject of your email'
-                    message = 'Body of your email'
-                    email = EmailMessage(
-                        subject,
-                        message,
-                        settings.DEFAULT_FROM_EMAIL,
-                        email_instance.emails,
-                        reply_to=[settings.DEFAULT_FROM_EMAIL],
-                    )
+                # Iterate over each unsent report and send an email for each file
+                for report in unsent_reports:
+                    # Check if the report template matches the user's template
+                    if report.template.user_type == user_type:
+                        # Iterate over each Email instance
+                        for email_instance in email_instances:
+                            # Create an EmailMessage object
+                            subject = 'Subject of your email'
+                            message = 'Body of your email'
+                            email = EmailMessage(
+                                subject,
+                                message,
+                                settings.DEFAULT_FROM_EMAIL,
+                                email_instance.emails,
+                                reply_to=[settings.DEFAULT_FROM_EMAIL],
+                            )
 
-                    # Get the file content from the ArchivedReport
-                    file_content = report.file.read()
+                            # Get the file content from the ArchivedReport
+                            file_content = report.file.read()
 
-                    # Attach the file to the email
-                    email.attach(report.file.name, file_content, 'application/octet-stream')
+                            # Attach the file to the email
+                            email.attach(report.file.name, file_content, 'application/octet-stream')
 
-                    # Send the email
-                    email.send()
+                            # Send the email
+                            email.send()
 
-                    # Update the sent status of the ArchivedReport
-                    report.sent = True
-                    report.save()
+                        # Update the sent status of the ArchivedReport
+                        report.sent = True
+                        report.save()
 
-        except Email.DoesNotExist:
-            print(f"Email with user_type {user_type} does not exist.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-# Example usage:
-send_emails_for_unsent_reports()
+            except Email.DoesNotExist:
+                print(f"Email with user_type {user_type} does not exist.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
