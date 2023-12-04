683ae438c9f89e80dcceb1296e989adb75d1c4db
iretes <i.testa@studenti.unipi.it>
2023-12-04 14:46:28 +0100

    radar plot fix

--- smartreport_app/kb_interface.py
+++ smartreport_app/kb_interface.py
@@ -27,15 +27,7 @@ def kb_interface(params):
         'rgb(255, 205, 86)',
         'rgb(201, 203, 208)'
     ]
-    transp_colors = [
-        'rgb(54, 162, 235, 0.2)',
-        'rgb(255, 98, 132, 0.2)',
-        'rgb(75, 193, 193, 0.2)',
-        'rgb(255, 159, 64, 0.2)',
-        'rgb(154, 102, 255, 0.2)',
-        'rgb(255, 205, 86, 0.2)',
-        'rgb(201, 203, 208, 0.2)'
-    ]
+    transp_colors = [color[:-1]+', 0.2)' for color in colors]
     frequencies_lbls = {
         'monthly': [
             'January',
@@ -75,50 +67,35 @@ def kb_interface(params):
 
     kb_resps = []
     for i, kpi_uid in enumerate(kpi_list):
-        kn_resp = get_kpi_value(kpi_uid, start_time, end_time ) # call to group 1 api
+        kn_resp = get_kpi_value(kpi_uid, start_time, end_time) # call to group 1 api
         kb_resps.append(kn_resp)
 
     frequencies_lbls['weekly'] = [f'Week {i}' for i in range(len(kb_resps[0]['value']))] # assuming they all have the same length
 
     labels = []
     datasets = []
-    if plot_type == 'radar':
-        for kb_resp in kb_resps:
-            labels.append(kb_resp['name'])
-        for i in range(len(kb_resps[0]['value'])): # assuming they all have the same length
-            dataset = {}
-            dataset['label'] = frequencies_lbls[frequency][(i+start_freq)%len(frequencies_lbls[frequency])]
-            dataset['data'] = [kb_resp['value'][i] for kb_resp in kb_resps]
-            dataset['fill'] = True
-            dataset['backgroundColor'] = transp_colors[i%len(colors)]
+    for i in range(len(kb_resps[0]['value'])): # assuming they all have the same length
+        labels.append(frequencies_lbls[frequency][(i+start_freq)%len(frequencies_lbls[frequency])])
+    for i, kb_resp in enumerate(kb_resps):
+        dataset = {}
+        dataset['label'] = kb_resp['name']
+        dataset['data'] = kb_resp['value']
+        if plot_type == 'line':
+            dataset['fill'] = False
             dataset['borderColor'] = colors[i%len(colors)]
-            dataset['pointBackgroundColor'] = colors[i%len(colors)]
-            dataset['pointBorderColor'] = '#fff'
-            dataset['pointHoverBackgroundColor'] = '#fff'
-            dataset['pointHoverBorderColor'] = colors[i%len(colors)]
-            datasets.append(dataset)
-    else:
-        for i in range(len(kb_resps[0]['value'])): # assuming they all have the same length
-            labels.append(frequencies_lbls[frequency][(i+start_freq)%len(frequencies_lbls[frequency])])
-        for i, kb_resp in enumerate(kb_resps):
-            dataset = {}
-            if plot_type == 'line':
-                dataset['label'] = kb_resp['name']
-                dataset['data'] = kb_resp['value']
-                dataset['fill'] = False
-                dataset['borderColor'] = colors[i%len(colors)]
-                dataset['tension'] = 0.1
-            elif plot_type == 'bar':
-                dataset['label'] = kb_resp['name']
-                dataset['data'] = kb_resp['value']
-                dataset['borderColor'] = colors[i%len(colors)]
-                dataset['backgroundColor'] = colors[i%len(colors)]
-                dataset['borderWidth'] = 1
-            elif plot_type == 'pie' or plot_type == 'doughnut':
-                dataset['label'] = kb_resp['name']
-                dataset['data'] = kb_resp['value']
-                dataset['backgroundColor'] = colors
-            datasets.append(dataset)
+            dataset['backgroundColor'] = transp_colors[i%len(colors)]
+            dataset['tension'] = 0.1
+        elif plot_type == 'bar':
+            dataset['borderColor'] = colors[i%len(colors)]
+            dataset['backgroundColor'] = colors[i%len(colors)]
+            dataset['borderWidth'] = 1
+        elif plot_type=='radar':
+            dataset['fill'] = True
+            dataset['borderColor'] = colors[i%len(colors)]
+            dataset['backgroundColor'] = transp_colors[i%len(colors)]
+        elif plot_type == 'pie' or plot_type == 'doughnut':
+            dataset['backgroundColor'] = colors
+        datasets.append(dataset)
 
     response = {
         'labels': labels,
